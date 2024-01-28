cbac77624db215a32386e8c7fb918bd7457a05a0
José A. Soares de Oliveira <91433859+hermengardo@users.noreply.github.com>
2024-01-28 01:39:39 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,10 +3,6 @@ const app = express();
 
 app.use(express.static('.'));
 
-app.get("/", function(req, res) {
-  res.sendFile(__dirname + "/index.html");
-});
-
 app.listen(8000, function() {
   console.log("Server is running on localhost:8000");
 });
